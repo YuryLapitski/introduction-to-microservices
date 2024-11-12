be903229105bb015622f8605319b85e783a2af39
Yury_Lapitski1 <yury_lapitski1@epam.com>
2024-11-12 23:02:48 +0500

    fix database creation issue

--- init-scripts-song/song.sql
+++ init-scripts-song/song.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS songs
+CREATE TABLE IF NOT EXISTS song
 (
     id         SERIAL PRIMARY KEY,
     name       VARCHAR(255),
     artist     VARCHAR(255),
     album      VARCHAR(255),
     length     VARCHAR(255),
-    resourceId VARCHAR(255),
+    resource_id VARCHAR(255),
     year       VARCHAR(255)
 );
